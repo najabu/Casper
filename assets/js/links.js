6350721634a9a493f5722f02fe04f75e68411eb8
Nathaniel Bomberger <nathaniel.bomberger@gmail.com>
2021-06-04 14:48:46 -0400

    Updated .gitignore

--- assets/js/links.js
+++ assets/js/links.js
@@ -2,9 +2,15 @@ function toggle(e) {
     if (e.classList.contains("fa-moon-o")) { //se tem lua
         e.classList.remove("fa-moon-o"); //remove classe icone lua
         e.classList.add("fa-sun-o"); //coloca classedo sol
-        e.style.color = "rgb(225, 225, 0)";
+        // e.style.color = "rgb(225, 225, 0)";
         document.body.style.background = 'rgb(10, 10, 10)';
         document.querySelector('#userName').style.color = '#fff';
+        
+        var titles = document.querySelectorAll('.grid-card-title');
+
+        for (let i = 0; i < titles.length; i++) {
+            titles[i].style.color = '#fff'
+        }
 
         let links = document.querySelectorAll('.link');
         // Pega apenas o primeiro do array
@@ -29,6 +35,11 @@ function toggle(e) {
         document.body.style.background = 'rgb(243, 242, 242)';
         document.querySelector('#userName').style.color = 'rgb(99, 99, 99)';
 
+        var titles = document.querySelectorAll('.grid-card-title');
+
+        for (let i = 0; i < titles.length; i++) {
+            titles[i].style.color = 'rgb(99, 99, 99)'
+        }
         let links = document.querySelectorAll('.link');
         // Pega apenas o primeiro do array
         links[0].style.filter = 'grayscale(0%)';
